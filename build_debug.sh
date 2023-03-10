7c2fd87d19891d36bde45be95d560885dbdcd017
RicKinG3 <90574265+RicKinG3@users.noreply.github.com>
2023-03-10 23:48:51 +0300

    Delete build_debug.sh

--- build_debug.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
- gcc -std=c99 -Wall -Werror -Wpedantic -Wvla -Wextra -Wfloat-equal -Wfloat-conversion -c main.c # Получаем объектный файл (компиляция)
- gcc -o app.exe main.o -lm -g # Получаем исполняемый файл c ключем -g3 для получение доп отладочной инфы (компановка)
